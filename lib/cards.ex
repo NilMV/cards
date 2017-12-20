59594b507f171196a68a9ae471f1091c4386889e
NilMV <nil.medvedev17@gmail.com>
2017-12-21 00:27:05 +0300

    first stepsadd

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,7 +1,18 @@
 defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  def hello do
-    "Hi there!"
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end  
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
   end
-  
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
 end
